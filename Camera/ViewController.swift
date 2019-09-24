a52ddc06ca88a3b1881df11aca45f9b92a2d8f81
takayuki1989 <shimada28@hotmail.co.jp>
2019-09-24 20:52:19 +0900

    01

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+        
+        let camera = UIImagePickerController.SourceType.camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(camera){
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        self.dismiss(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
